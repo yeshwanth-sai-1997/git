a842df6f4ea0a3e74dec3589f27de72e57d87c24
yeshwanth-sai-1997 <saiyeshwanth382@gmail.com>
2026-07-03 10:15:16 +0530

    version1.0

--- yeshwanth.js
+++ yeshwanth.js
@@ -1,4 +1,4 @@
 console.log("Hello, yeshwanth!");
-console.log("How are you, yeshwanth?");
+console.log("How are you, yeshwanth!");
 console.log("I hope you are doing well, yeshwanth!");
 console.log("Have a great day, yeshwanth!");
